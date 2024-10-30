ef4fb580b52eeb785dcb9b51b182d2cb1a54a00a
Hank <lovebaonvwu@gmail.com>
2024-10-30 16:49:01 +0800

    add teachers list

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,3 +1,20 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 export default function Page() {
-  return <>Parent Page</>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left  */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (Hank Zheng)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right  */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 }
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "@/lib/data";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
 export default function BigCalendar() {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
   return (
-    <div>
-      <Calendar
-        localizer={localizer}
-        events={calendarEvents}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-    </div>
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      min={new Date(2025, 0, 1, 8, 0, 0)}
+      max={new Date(2025, 0, 1, 17, 0, 0)}
+    />
   );
 }
